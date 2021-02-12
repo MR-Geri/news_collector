82c2282315f734f0a4dcab0eecb3a192487ebea9
MR-Geri <ilya.kamckine@ya.ru>
2021-02-12 21:21:14 +0300

    сайт

--- temp.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-if __name__ == '__main__':
-    url = 'https://3dnews.ru/assets/external/illustrations/2021/02/11/1032481/00.jpg'
-    with open('temp.jpg', 'wb') as file:
-        file.write(requests.get(url).content)
