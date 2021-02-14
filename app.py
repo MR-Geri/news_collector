7149450b4fee149edff4c94dc567642ebc1cfe1f
MR-Geri <ilya.kamckine@ya.ru>
2021-02-14 23:11:09 +0300

    update

--- app.py
+++ app.py
@@ -14,6 +14,8 @@ class Article(db.Model):
     intro = db.Column(db.String(300), nullable=False)
     text = db.Column(db.Text, nullable=False)
     date = db.Column(db.DateTime, default=datetime.datetime.utcnow)
+    flag = db.Column(db.Boolean, default=False)
+    url = db.Column(db.Text, default='')
 
     def __repr__(self):
         return '<Article %r>' % self.id
@@ -31,8 +33,14 @@ def about():
 
 @app.route('/posts')
 def posts():
-    articles = Article.query.order_by(Article.date).all()
+    articles = Article.query.order_by(Article.date.desc()).all()
     return render_template('posts.html', articles=articles)
+
+
+@app.route('/posts/<int:id>')
+def post_detail(id):
+    article = Article.query.get(id)
+    return render_template('post_detail.html', article=article)
 
 
 if __name__ == '__main__':
