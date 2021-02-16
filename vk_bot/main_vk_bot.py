1cd19661466d40d6af54bf19faa3b6a867d45fa7
ilyak <ilya.kamckine@ya.ru>
2021-02-16 19:40:14 +0300

    update

--- vk_bot/main_vk_bot.py
+++ vk_bot/main_vk_bot.py
@@ -95,7 +95,7 @@ class LocalBot:
                     for url in post[6].split('\n'):
                         url = url.rstrip()
                         if url.split('.')[-1] in IMAGE_EXTENSION:
-                            self.upload_image(path_file='../posts/', url=url)
+                            self.upload_image(path_file='posts/', url=url)
                             photo_id = self.data[0]['id']
                             photos += f'photo{self.data[0]["owner_id"]}_{photo_id},'
                             os.remove('posts/' + url.split('/')[-1])
