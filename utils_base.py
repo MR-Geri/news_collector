df7c8d4b67e5c98d97324a8755f58f601e072c6f
ilyak <ilya.kamckine@ya.ru>
2021-02-16 19:33:40 +0300

    update

--- utils_base.py
+++ utils_base.py
@@ -16,20 +16,20 @@ def get_base(file: str, is_commit: bool = False):
 
 
 def get_id() -> list:
-    with get_base('../posts.sqlite') as base:
+    with get_base('posts.sqlite') as base:
         return [i[0] for i in base.execute("""SELECT id FROM article;""").fetchall()]
 
 
 def get_post(id_: int) -> list:
-    with get_base('../posts.sqlite') as base:
+    with get_base('posts.sqlite') as base:
         return base.execute("""SELECT * FROM article WHERE id = ?;""", (id_, )).fetchall()[0]
 
 
 def get_no_push_posts() -> list:
-    with get_base('../posts.sqlite') as base:
+    with get_base('posts.sqlite') as base:
         return base.execute("""SELECT * FROM article WHERE flag = 0;""").fetchall()
 
 
 def set_post_true(id_: int) -> None:
-    with get_base('../posts.sqlite', True) as base:
+    with get_base('posts.sqlite', True) as base:
         base.execute("""UPDATE article SET flag = 1 WHERE id = ?""", (id_, ))
